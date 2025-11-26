105c7e53bc32cc9feb945ea5c135e3d2c5a5cfce
Sergio444757 <9028570@student.zadkine.nl>
2025-11-26 15:47:16 +0100

    nieuwe
    update

---
+++ opdracht-1/main.js
@@ -0,0 +1,7 @@
+
+        naam = prompt("Wat is jouw naam?");
+        alert("Hallo " + naam + ", welkome op mijn website!");
+        document.write("sergio whitfield");
+
+        document.getElementById("title").style.color = "red";
+        document.getElementById("title").innerHTML = naam;
