4a710bc0ba63165a5d1137ab4f4cb77799ccd17d
Sergio444757 <9028570@student.zadkine.nl>
2025-12-04 13:56:38 +0100

    Add files via upload

---
+++ opdracht-10/main.js
@@ -0,0 +1,15 @@
+function groet_ochtend(){
+document.getElementById("title").innerText = "Goedemorgen";
+document.body.style.backgroundColor = "lightyellow";
+};
+
+function groet_middag(){
+document.getElementById("title").innerText = "Goedemiddag";
+  document.body.style.backgroundColor = "orange";
+};
+
+function groet_avond(){
+document.getElementById("title").innerText = "Goedeavond";
+  document.body.style.backgroundColor = "darkblue"
+
+};
