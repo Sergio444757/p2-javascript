921d4f6a5323143a575e93a9bbec29ee1af57315
Sergio444757 <9028570@student.zadkine.nl>
2025-12-04 13:07:22 +0100

    Add files via upload

---
+++ opdracht-9/main.js
@@ -0,0 +1,11 @@
+function verberg_tekst() {
+    document.getElementById("title").style.display="none"
+    
+}
+
+function toon_tekst() {
+     
+     document.getElementById("title").style.display="block"
+
+}
+
